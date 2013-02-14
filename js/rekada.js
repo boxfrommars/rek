501fbc0d0af936f6bbbbd635f337c8b37b1ce85c
Alex <abykovskih@gmail.com>
2013-02-14 19:15:58 +0400

    мой второй коммит

---
+++ js/rekada.js
@@ -0,0 +1,11 @@
+/*
+$(document).ready(function(){
+   $('.prod').hover(function() {
+    $(this).parent('.prod').find('div').addClass("hover");
+  },
+  function () {
+    $(this).parent('.prod').find('.hover').removeClass("hover");
+  }
+);
+  }); 
+*/
