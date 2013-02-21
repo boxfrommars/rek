3bc7cdfbaeff8b97e49590b044aa3d817e55df29
Alex <abykovskih@gmail.com>
2013-02-21 10:16:37 +0400

    support ie8+

--- js/rekada.js
+++ js/rekada.js
@@ -1,11 +1,5 @@
 /*
-$(document).ready(function(){
-   $('.prod').hover(function() {
-    $(this).parent('.prod').find('div').addClass("hover");
-  },
-  function () {
-    $(this).parent('.prod').find('.hover').removeClass("hover");
-  }
-);
-  }); 
-*/
+ *$(document).ready(function(){
+	$('.look').lightBox();
+}); 
+ */
