41188d388e4b7c13b8401eda0422c13d929fd759
Alex <abykovskih@gmail.com>
2013-02-25 18:52:14 +0400

    исправление доп меню

--- js/rekada.js
+++ js/rekada.js
@@ -1,5 +1,34 @@
-/*
- *$(document).ready(function(){
-	$('.look').lightBox();
-}); 
- */
+$(document).ready(function(){
+   $('#toggle_1').click(function() {
+        $('.hide_block').hide();
+        $('#granit').show();
+        });
+   $('#toggle_2').click(function() {
+        $('.hide_block').hide();
+        $('#fasad').show();
+   });
+   $('#toggle_3').click(function() {
+        $('.hide_block').hide();
+        $('#kamen').show();
+   });
+   $('#toggle_4').click(function() {
+        $('.hide_block').hide();
+        $('#rezka').show();
+    });
+});
+
+$(document).click(function(e){
+    if ($(e.target).parents().filter('#toggle_1:visible').length != 1) {
+        $('#granit').hide();
+    };
+     if ($(e.target).parents().filter('#toggle_2:visible').length != 1) {
+        $('#fasad').hide();
+    };
+    if ($(e.target).parents().filter('#toggle_3:visible').length != 1) {
+        $('#kamen').hide();
+    };
+    if ($(e.target).parents().filter('#toggle_4:visible').length != 1) {
+        $('#rezka').hide();
+    }
+});
+
