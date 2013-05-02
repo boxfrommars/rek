150887db867dc5042113cff27e2de83f50c335a2
Dmitry Groza <boxfrommars@gmail.com>
2013-05-02 19:33:40 +0400

    изм

--- deploy/4-page.sql
+++ deploy/4-page.sql
@@ -7,21 +7,20 @@ CREATE TABLE "page" (
   "title" VARCHAR(255) NOT NULL,
   "content" TEXT,
 
-  "page_url" VARCHAR(255),
+  "page_url" VARCHAR(255) UNIQUE,
   "page_title" VARCHAR (255),
   "page_description" TEXT,
   "page_keywords" TEXT,
 
   "name" VARCHAR(255) UNIQUE,
   "is_locked" BOOL DEFAULT 'f',
---   "path" LTREE,
+  "path" LTREE,
   "created_at" TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NOW(),
   "updated_at" TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NOW(),
-  PRIMARY KEY("id"),
-  UNIQUE ("page_url")
+
+  PRIMARY KEY("id")
 );
 
 CREATE INDEX ON page USING GIST (path);
-INSERT INTO "page" (is_published, title, content, page_url, page_title, is_locked, name) VALUES ('t', 'Главная', '', '', 'Главная', 't', 'Top', 'main');
-INSERT INTO "page" (is_published, title, content, page_url, page_title, is_locked, name) VALUES ('t', 'Галерея', '', 'gallery', 'Галерея', 't', 'Top.Second', 'gallery');
-
+INSERT INTO "page" (is_published, title, content, page_url, page_title, is_locked, path, name) VALUES ('t', 'Главная', '', '', 'Главная', 't', 'Top', 'main');
+INSERT INTO "page" (is_published, title, content, page_url, page_title, is_locked, path, name) VALUES ('t', 'Галерея', '', 'gallery', 'Галерея', 't', 'Top.Second', 'gallery');
