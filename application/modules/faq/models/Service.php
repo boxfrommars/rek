a7a5587312a5f5d9ea03772a5d7267fa7f326104
Dmitry Groza <boxfrommars@gmail.com>
2013-05-02 19:29:24 +0400

    rm

--- application/modules/faq/models/Service.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @copyright  (c) 2013
- * @author Franky Calypso <franky.calypso@gmail.com>
- */
-class Faq_Model_Service extends Whale_Db_TableCached
-{
-    protected $_name = 'faq';
-}
