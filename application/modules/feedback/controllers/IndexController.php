756aa3307e3f303a9e56371dce9ad2464b3594b6
Dmitry Groza <boxfrommars@gmail.com>
2013-07-01 12:10:05 +0400

    настройки

--- application/modules/feedback/controllers/IndexController.php
+++ application/modules/feedback/controllers/IndexController.php
@@ -35,9 +35,9 @@ class Feedback_IndexController extends Whale_Controller_Action_Admin_Article
         Whale_Log::log($this->_settings);
         $mail = new Zend_Mail('UTF-8');
         $mail->setBodyText(implode("\n\n", $values));
-        $mail->setFrom($this->_settings['email'], 'Админ');
+        $mail->setFrom($this->_settings['email'], 'Admin');
 
-        $mail->addTo($this->_settings['email'], 'Админ');
+        $mail->addTo($this->_settings['email'], 'Admin');
         $mail->setSubject('Новая заявка на сайте Рекада');
         Whale_Log::log($mail->getParts());
         $mail->send();
