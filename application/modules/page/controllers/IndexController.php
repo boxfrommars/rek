fdb49f20902e6be3bf321bafb1cdc5dbd0d9c4ad
Dmitry Groza <boxfrommars@gmail.com>
2013-08-05 18:37:09 +0400

    Вынесение первого уровня статей в сайдбар

--- application/modules/page/controllers/IndexController.php
+++ application/modules/page/controllers/IndexController.php
@@ -6,7 +6,7 @@
 class Page_IndexController extends Whale_Controller_Action
 {
     /**
-     * @var Gallery_Model_Service
+     * @var Page_Model_Service
      */
     protected $_service;
 
@@ -23,7 +23,11 @@ class Page_IndexController extends Whale_Controller_Action
         $layout = Zend_Layout::getMvcInstance();
         $layout->setLayout('page');
         $this->view->assign($this->getParam('page'));
-        Whale_Log::log($this->getAllParams());
+        if ($page['entity'] === 'page' && !empty($page['parents'][1]['id'])) {
+            $pagesMenuItems = $this->_service->getBaseSelect()->where('p.entity = ?', 'page')->where('p.id_parent = ?', $page['parents'][1]['id'])->where('p.is_published')->query()->fetchAll();
+            Whale_Log::log($pagesMenuItems);
+            $this->view->pagesMenuItems = $pagesMenuItems;
+        }
     }
 }
 
